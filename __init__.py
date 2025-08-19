791cddaf50cf1c52a230b36c0a1dd53f57a33960
Vsolon <vsolonlesammich@gmail.com>
2025-08-19 11:54:41 -0400

    Initalize As Node Template Instead of UI Template

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for cbz_pack."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """Vsolon"""
+__email__ = "you@gmail.com"
+__version__ = "0.0.1"
+
+from .src.cbz_pack.nodes import NODE_CLASS_MAPPINGS
+from .src.cbz_pack.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
